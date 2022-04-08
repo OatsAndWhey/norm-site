97c0c96346150326bec8c2e48929f6335ad90984
Rob Hirst <robahirst@gmail.com>
2022-04-07 19:23:25 -0700

    fix scaling

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'We Love Norm', 
+    title: 'Home', 
     firstword: 'We',
     secondword: 'love',
     thirdword: 'Norm',
